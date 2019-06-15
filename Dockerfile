d898b693042956e652eaeca9181ac50742cebd6b
Sachin Chandra <chandrasachin16@yahoo.co.in>
2019-06-15 19:41:17 +0530

    Added the first code

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+From java:8
+copy ./target/spring-boot-example-0.0.1-SNAPSHOT.jar spring-boot-example-0.0.1-SNAPSHOT.jar
+CMD ["java","-jar","spring-boot-example-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/te/sachin/spring/boot/HomeController.java
@@ -0,0 +1,19 @@
+package com.te.sachin.spring.boot;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+public class HomeController {
+	
+	@RequestMapping(method=RequestMethod.GET,value="/hello/s1")  
+    public String hello(){  
+        return"Hello sachin!";  
+    }  
+	@RequestMapping(method=RequestMethod.GET,value="/hello/s2")  
+    public String hello1(){  
+        return"Hello sachin!";  
+    }  
+
+}
